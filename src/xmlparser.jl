ef6a9c45975a3f70429143b737d12439d9634b8b
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-01-10 17:27:30 +0100

    Created a separate function for the depth first traversal of all of the nodes of the tree.

--- src/xmlparser.jl
+++ src/xmlparser.jl
@@ -28,6 +28,37 @@ function the_streaming_way_of_doing_things()
 
 end
 
+
+
+# traverse all nodes of the XML tree in depth first fashion
+# without recursion
+# Initialize the deque with the children of the root
+# daarna halen we er steeds 1 op
+# Mocht dat een element zijn
+# dan zetten we de kinderen daarvan weer op de deck.
+# NOTE: maybe a stack is better here?
+# for now we create an array with the result
+# implementing it as an iterator would be nicer
+function create_an_array_of_the_xml_nodes(root)
+    result = Union{AbstractString, ETree}[]
+    deck = deque(Union{AbstractString, ETree})
+    for element in root.elements
+        push!(deck, element)
+    end
+
+    while !isempty(deck)
+        a = popfirst!(deck)
+        push!(result, a)
+        if typeof(a) == ETree
+            for element in reverse(a.elements)
+                pushfirst!(deck, element)
+            end
+        end
+    end
+    return result
+end
+
+
 xml = "<xml>Mondays are <del>well good</del><add>def bad</add>!</xml>"
 # the_streaming_way_of_doing_things()
 
@@ -60,39 +91,20 @@ struct StartNode <: VariantGraphNode end
 vg = VariantGraphNode[]
 push!(vg, StartNode())
 
-#TODO: dit moet recursief om alle elementen te processen...
-# ik hou niet zo van recursie, kan dat echt niet anders?
-# Er zijn wel generators in julia
-# lightxml liberary lijkt wel een iterator te hebben
-# dat is ook alleen maar van de directe kinderen
-# misschien een priority queue vullen?
-# waarbij bij het aflopen van de queue steeds weer nodes op de queue worden gezet.
 
 
-# We zetten alle kinderen van de root op een deck
-# daarna halen we er steeds 1 op
-# Mocht dat een element zijn
-# dan zetten we de kinderen daarvan weer op de deck.
 
-deck = deque(Union{AbstractString, ETree})
-for element in root.elements
-    push!(deck, element)
-end
-println(deck)
 
-while !isempty(deck)
-    a = popfirst!(deck)
-    if typeof(a) == String
-        println(a)
-    elseif typeof(a) == ETree
-        println(a)
-        for element in reverse(a.elements)
-            pushfirst!(deck, element)
-        end
+
+
+all_nodes = create_an_array_of_the_xml_nodes(root)
+for node in all_nodes
+    if typeof(node) == String
+        println(node)
+    elseif typeof(node) == ETree
+        println(node.name)
     end
 end
-
-
 
 
 
