21124ce0962ce84ee792be4ed8ccec006b400562
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-01-10 14:55:44 +0100

    Some small steps towards graph building.

--- src/xmlparser.jl
+++ src/xmlparser.jl
@@ -27,7 +27,7 @@ function the_streaming_way_of_doing_things()
 
 end
 
-xml = "<xml> text </xml>"
+xml = "<xml>Mondays are <del>well good</del><add>def bad</add>!</xml>"
 # the_streaming_way_of_doing_things()
 
 root = xp_parse(xml)
@@ -38,9 +38,46 @@ root = xp_parse(xml)
 # Zijn er traversals ofzo?
 
 
+# wat we willen doen is alle text elementen uit de xml tree halen
+# en die dan in een variant graph achtige constructie plaatsen
+# Zoals elke variant graph is er een start en end vertex
+# Eigenlijk moeten we hier ook tokenizen
+# dus dan na de start node zijn er twee text nodes
+# dan een divergence node
+# met twee text nodes (well good) aan de ene kant
+# en twee texdt nodes (def bad) ana de andere kant.
+# had de java versie al divergence en convergence nodes?
+
+# moet ik zelf een graph struct aanmaken?
+# of een liberary daarvoor gebruiken
+# mijn eerste gevoel is om het gewoon even zelf te doen
+# even wat simpele structs te definieren
+abstract type VariantGraphNode end
+struct TextNode <: VariantGraphNode end
+struct StartNode <: VariantGraphNode end
+
+vg = VariantGraphNode[]
+push!(vg, StartNode())
+
+
 for a in root.elements
-    println(a)
+    if typeof(a) == String
+        println(a)
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
