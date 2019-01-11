3534626049714aed8ab7b05b7a72d2c541c04c91
rhdekker <ronald.dekker@di.huc.knaw.nl>
2019-01-11 10:15:57 +0100

    Created a main function; made bla() function less dependent on create_an_array_of_the_xml_nodes function.

--- src/xmlparser.jl
+++ src/xmlparser.jl
@@ -56,7 +56,7 @@ function create_an_array_of_the_xml_nodes(root)
     return result
 end
 
-function bla()
+function bla(all_nodes)
     println("Called")
     vg = VariantGraphNode[]
     startNode = StartNode()
@@ -65,7 +65,6 @@ function bla()
     vgedges = []
     last_node = startNode
 
-    all_nodes = create_an_array_of_the_xml_nodes(root)
     for node in all_nodes
         if typeof(node) == String
             println(node)
@@ -84,39 +83,45 @@ function bla()
 end
 
 
-xml = "<xml>Mondays are <del>well good</del><add>def bad</add>!</xml>"
-# the_streaming_way_of_doing_things()
-
-root = xp_parse(xml)
-# dump(root)
-
-# ik moet een methode hebben die gewoon over alle elements itereert.
-# maar het eerste etree element is al meteen de root..
-# Zijn er traversals ofzo?
-
-
-# wat we willen doen is alle text elementen uit de xml tree halen
-# en die dan in een variant graph achtige constructie plaatsen
-# Zoals elke variant graph is er een start en end vertex
-# Eigenlijk moeten we hier ook tokenizen
-# dus dan na de start node zijn er twee text nodes
-# dan een divergence node
-# met twee text nodes (well good) aan de ene kant
-# en twee texdt nodes (def bad) ana de andere kant.
-# had de java versie al divergence en convergence nodes?
-
-# moet ik zelf een graph struct aanmaken?
-# of een liberary daarvoor gebruiken
-# mijn eerste gevoel is om het gewoon even zelf te doen
-# even wat simpele structs te definieren
 abstract type VariantGraphNode end
 struct TextNode <: VariantGraphNode end
 struct StartNode <: VariantGraphNode end
 
-bla()
 
 
+function main()
+    xml = "<xml>Mondays are <del>well good</del><add>def bad</add>!</xml>"
+    # the_streaming_way_of_doing_things()
 
+    root = xp_parse(xml)
+    # dump(root)
+
+    all_nodes = create_an_array_of_the_xml_nodes(root)
+    bla(all_nodes)
+
+    # OUDE comments
+    # ik moet een methode hebben die gewoon over alle elements itereert.
+    # maar het eerste etree element is al meteen de root..
+    # Zijn er traversals ofzo?
+
+
+    # wat we willen doen is alle text elementen uit de xml tree halen
+    # en die dan in een variant graph achtige constructie plaatsen
+    # Zoals elke variant graph is er een start en end vertex
+    # Eigenlijk moeten we hier ook tokenizen
+    # dus dan na de start node zijn er twee text nodes
+    # dan een divergence node
+    # met twee text nodes (well good) aan de ene kant
+    # en twee texdt nodes (def bad) ana de andere kant.
+    # had de java versie al divergence en convergence nodes?
+
+    # moet ik zelf een graph struct aanmaken?
+    # of een liberary daarvoor gebruiken
+    # mijn eerste gevoel is om het gewoon even zelf te doen
+    # even wat simpele structs te definieren
+end
+
+main()
 
 
 
